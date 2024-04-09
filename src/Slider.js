65fd58e613ccb883717d2c222a324bc4c01ce9c0
Artem <blbo@yandex.ru>
2024-04-09 16:40:23 +0400

    0.0.5
    Слайдер не работает

--- src/Slider.js
+++ src/Slider.js
@@ -2,32 +2,63 @@ import React from 'react';
 import Image from './components/Image';
 import imgSlide from './images/slider/slide-1.png';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import './css/style.css';
 
-class Slider extends React.Component {
-    render() {
-        return (
-            <section className="slider">
-                <div className="container">
-                    <div className="swiper">
-                    <div className="swiper-wrapper">
-                        <div className="swiper-slide">
-                            <Image image={imgSlide} className="slider__img"/>
-                            <h2 className="slider__title">Доставка бесплатно от 1000 ₽</h2>
-                        </div>
-                        <div className="swiper-slide">
-                            <Image image={imgSlide} className="slider__img"/>
-                            <h2 className="slider__title">Скидка 5% на вторую доставку</h2>
-                        </div>
-                        <div className="swiper-slide">
-                            <Image image={imgSlide} className="slider__img"/>
-                            <h2 className="slider__title">Скидка 10% на третью и последующую доставку</h2>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-                </section>
-            )
-    }
-}
+// function Slider() {
+//     return (
+//       <Swiper
+//         spaceBetween={50}
+//         slidesPerView={1}
+//         loop={true}
+//         grabCursor={true}
+//         speed={1500}
+//         autoplay={{delay : 1000, disableOnInteraction : true}}
+//         >
 
-export default Slider;
+//         <section className="slider">
+//             <div className="container">
+//                 <div className="swiper">
+//                     <div className="swiper-wrapper">
+//                         <SwiperSlide>
+//                             <Image image={imgSlide} className="slider__img"/>
+//                             <h2 className="slider__title">Доставка бесплатно от 1000 ₽</h2>
+//                         </SwiperSlide>
+//                         <SwiperSlide>
+//                             <Image image={imgSlide} className="slider__img"/>
+//                             <h2 className="slider__title">Скидка 5% на вторую доставку</h2>
+//                         </SwiperSlide>
+//                         <SwiperSlide>
+//                             <Image image={imgSlide} className="slider__img"/>
+//                             <h2 className="slider__title">Скидка 10% на третью и последующую доставку</h2>
+//                         </SwiperSlide>
+//                     </div>
+//                 </div>
+//             </div>
+//         </section>
+        
+//       </Swiper>
+//     );
+//   };
+
+// export default Slider;
+
+export default () => {
+    return (
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        loop={true}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+        autoplay={{delay : 1000, disableOnInteraction : true}}
+        speed={1500}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+    );
+  };
--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import Header from './Header';
 import Slider from './Slider';
 import './css/style.css';
-import './css/swiper.css';
-
-//необходимо подключить скрипты свайпера
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
