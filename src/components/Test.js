c6f749392dcaa61b0a967718d5038c850e7d8bbe
Artem <blbo@yandex.ru>
2024-04-24 14:45:39 +0400

    0.2.5 - test
    Вывод товаров через map

--- src/components/Test.js
+++ src/components/Test.js
@@ -12,8 +12,45 @@ class Stock extends Component {
                             <a className="product-list__all" href="#">Все товары</a>
                         </div>
                         <div className="product-list__box">
-                            {this.props.ArrProducts.map(el => (
-                                <h1 key={el.id}>{el.id}</h1>))}
+                            
+                        {this.props.ArrProducts.map(el => (
+
+                            <div className="product" key={el.id}>
+                                <div className="product__box-images">
+                                    <a className="product__link" href="#">
+                                        <img src={el.imgProduct} className="product__images" alt="Изображение продукта"/>
+                                    </a>
+                                    <button className="product__button-favorites" type="button"></button>
+                                    <span className="product__sales">{el.productSales}</span>
+                                </div>
+                                <div className="product__box-price">
+                                    <div className="product__price">
+                                        <span className="product__price-cart">{el.productPriceCart} ₽</span>
+                                        <span className="product__title">С картой</span>
+                                    </div>
+                                    <div className="product__price">
+                                        <span className="product__price-nocart">{el.productNoCart} ₽</span>
+                                        <span className="product__title">Обычная</span>
+                                    </div>
+                                </div>
+                                <p className="product__name">
+                                    <a className="product__link" href="#">{el.productName}</a>
+                                </p>
+                                <div className="product__box-rating">
+                                    <button className="product__star product__star_on" type="button"></button>
+                                    <button className="product__star product__star_on" type="button"></button>
+                                    <button className="product__star product__star_on" type="button"></button>
+                                    <button className="product__star product__star_on" type="button"></button>
+                                    <button className="product__star" type="button"></button>
+                                </div>
+                                <button className="product__button-buy" type="button">В корзину</button>
+                            </div>
+    
+                            ))}
+
+                        
+
+
                         </div>
                     </div>
                 </div>
@@ -24,5 +61,8 @@ class Stock extends Component {
 
 export default Stock;
 
-
+// <div className="product-list__box">
+// {this.props.ArrProducts.map(el => (
+//     <h1 key={el.id}>{el.id}</h1>))}
+// </div>
 
